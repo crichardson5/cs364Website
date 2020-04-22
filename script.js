f39e142ff9fdf4d8368aa93e866224d4073b19a3
Sai-Reddy21 <54084108+Sai-Reddy21@users.noreply.github.com>
2020-04-21 21:22:35 -0600

    added JS to login page

--- script.js
+++ script.js
@@ -2,29 +2,20 @@ function Validate_Info_Form_Data(){
   //Set errorsWithForm to False
   var errorsWithForm = true;
 
-var emailAddress = document.forms["loginPage364"]["emailAddress"].value;
+ var emailAddress = document.forms["loginPage364"]["emailAddress"].value;
   if (emailAddress == "") {
     alert("Email Address name must be filled out");
     errorsWithForm = false;
     return errorsWithForm;
   }
-
-  var password= document.forms["loginPage364"]["password"].value;
+  var password = document.forms["loginPage364"]["password"].value;
   if (password == "") {
     alert("Password must be filled out");
     errorsWithForm = false;
     return errorsWithForm;
    }
   
- 
-
-var letters = /^[A-Za-z\s\-]+$/;
-
-if(!(letters.test(password)))
-{
-errorsWithForm = false;
-return errorsWithForm;
-}
+ var letters = /^[A-Za-z\s\-]+$/;
 
  if(password.length < 1)  {  	
         errorsWithForm = false;
@@ -32,25 +23,24 @@ return errorsWithForm;
       }
  if(password.length > 50) {  	
         errorsWithForm = false;
+        alert("Password has exceeded limit.");
 	return errorsWithForm;  	
       }
-
 if(emailAddress.length < 1)  {  	
         errorsWithForm = false;
 	return errorsWithForm;  	
       }
  if(emailAddress.length > 50) {  	
         errorsWithForm = false;
+        alert("Email address has exceeded limit.");
 	return errorsWithForm;  	
       }
 
-
-}
 var emailHitter = /^[A-Za-z0-9_.-]+[@]{1}[A-Za-z0-9_.-]+$/;
-var getTested = 0;
 if(!(emailHitter.test(emailAddress)))
 {
 errorsWithForm = false;
+alert("Not a valid email address.");
 return errorsWithForm;
 }
 
