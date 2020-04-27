6461037173124ea440d12d488db17d8091b49859
crichardson5 <crichardson5@verizon.net>
2020-04-27 18:24:40 -0500

    final submission for first website milestone

--- loginPage364.php
+++ loginPage364.php
@@ -18,18 +18,20 @@ $error = false;
 <h1> Welcome to the Personal Finance Tracker</h1>
   <div class="loginBox">
   <img src="genericAvatar.png" class="avatar">
-    <h2> Login Here</h2>
+    <h2 class="loginh2"> Login Here</h2>
+	
 	<div class= "form">
-    <form name="loginPage364" action="index.html" method="POST" onSubmit = "return Validate_Info_Form_Data()">
-    <p><span class= "userInfo">Email Address</span></p>
-    <input type="text" id="emailAddress" name="emailAddress" placeholder="Enter Email Address"
-    value="<?php if (isset($_POST["username"]))
+		<form class="loginForm" name="loginPage364" action="index.html" method="POST" onSubmit = "return Validate_Info_Form_Data()">
+			<p><span class= "userInfo">Email Address</span></p>
+			<input type="text" id="emailAddress" name="emailAddress" placeholder="Enter Email Address"
+			value="<?php if (isset($_POST["username"]))
                             echo $_POST["username"]; ?>"/>
-    <p><span class= "userInfo">Password</span></p>
-    <input type="password" id="password" name="password" placeholder="Enter Password" />
-    <input type="submit" name="submit" value="Login" />
-    <? php echo $errorMessage; ?>
-       </form>
+
+			<p><span class= "userInfo">Password</span></p>
+			<input type="password" id="password" name="password" placeholder="Enter Password" /><br><br>
+			<input type="submit" name="submit" value="Login" />
+			<? php echo $errorMessage; ?>
+		</form>
     </div>
   </div>
 <script src="script.js"></script>
