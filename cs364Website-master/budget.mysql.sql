c00cf2812690b8838ea1fd0db04aedd404a3c34c
crichardson5 <54080403+crichardson5@users.noreply.github.com>
2020-05-20 17:32:18 -0500

    Update budget.mysql.sql

--- cs364Website-master/budget.mysql.sql
+++ cs364Website-master/budget.mysql.sql
@@ -24,6 +24,7 @@ CREATE TABLE userBudget (
 	budget_id INTEGER AUTO_INCREMENT,
 	user_id INTEGER NOT NULL,
 	budget_month INTEGER NOT NULL,
+	budget_year INTEGER NOT NULL,
 	
 	PRIMARY KEY (budget_id),
 	FOREIGN KEY (user_id) REFERENCES systemUser (id)
@@ -32,10 +33,13 @@ CREATE TABLE userBudget (
 
 CREATE TABLE budgetCategory (
 	name CHARACTER VARYING (32), 
+	budget_id INTEGER,
 	amount_allocated INTEGER NOT NULL,
 	amount_spent INTEGER NOT NULL,
 	
-	PRIMARY KEY (name)
+	PRIMARY KEY (name),
+	FOREIGN KEY (budget_id) REFERENCES userBudget(budget_id)
+		ON UPDATE CASCADE ON DELETE RESTRICT
 );
 
 CREATE TABLE budgetTransaction (
@@ -52,5 +56,21 @@ CREATE TABLE budgetTransaction (
 		ON UPDATE CASCADE ON DELETE RESTRICT
 );
 
-INSERT INTO systemUser (first_name, last_name, emailAddress, password) VALUES 
-('John', 'Doe', 'test@com', (SELECT sha1('test')));
+INSERT INTO systemUser VALUES 
+('1', 'John', 'Doe', 'test@com', (SELECT sha1('test')));
+
+INSERT INTO userBudget VALUES
+('1', '1', '05', '2020');
+
+INSERT INTO budgetCategory VALUES
+('Housing', '1', '50', '50'),
+('Transportation', '1', '50', '50'),
+('Food', '1', '50', '50'),
+('Utilities', '1', '50', '50'),
+('Insurance', '1', '50', '50'),
+('Debt Payments', '1', '50', '50'),
+('Investing', '1', '50', '50'),
+('Saving', '1', '50', '50'),
+('Personal', '1', '50', '50'),
+('Recreation', '1', '50', '50'),
+('Miscellaneous', '1', '50', '50');
