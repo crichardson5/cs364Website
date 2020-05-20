e199a8743b4fe75c492cf079636c2c8460e3b349
crichardson5 <54080403+crichardson5@users.noreply.github.com>
2020-05-20 18:07:42 -0500

    Update viewCreateBudget.php

--- cs364Website-master/viewCreateBudget.php
+++ cs364Website-master/viewCreateBudget.php
@@ -1,4 +1,4 @@
-<?php include ('authenticate.php')
+<?php include ('authenticate.php');
  if(isset($_POST["submitTransaction"])){
 	if(isset($_POST["category"])){
 		if(isset($_POST["amount"])){
@@ -6,6 +6,10 @@
 		}
 	}
  }
+
+$connection = new mysqli("localhost", "student", "CompSci364",
+                         "budget");
+
 ?>
 ﻿<!DOCTYPE html>
 <html lang - "en">
@@ -36,85 +40,45 @@
 		
 		<br>
 		
-		<table class="viewTable">
-			<tr>
-				<th>Category</th>
-				<th>Budgeted</th>
-				<th>Spent</th>
-				<th>Remaining</th>
-			</tr>
-			<tr>
-				<td>Housing</td>
-				<td>$100</td>
-				<td>$0</td>
-				<td>$100</td>
-			</tr>
-			<tr>
-				<td>Transportation</td>
-				<td>$100</td>
-				<td>$0</td>
-				<td>$100</td>
-			</tr>
-			<tr>
-				<td>Food</td>
-				<td>$100</td>
-				<td>$0</td>
-				<td>$100</td>				
-			</tr>
-			<tr>
-				<td>Utilities</td>
-				<td>$100</td>
-				<td>$0</td>
-				<td>$100</td>				
-			</tr>
-				<td>Insurance</td>
-				<td>$100</td>
-				<td>$0</td>
-				<td>$100</td>
-			<tr>
-				<td>Debt Payments</td>
-				<td>$100</td>
-				<td>$0</td>
-				<td>$100</td>
-			</tr>
-			<tr>
-				<td>Investing</td>
-				<td>$100</td>
-				<td>$0</td>
-				<td>$100</td>				
-			</tr>
-			<tr>
-				<td>Saving</td>
-				<td>$100</td>
-				<td>$0</td>
-				<td>$100</td>				
-			</tr>
-			<tr>
-				<td>Personal</td>
-				<td>$100</td>
-				<td>$0</td>
-				<td>$100</td>				
-			</tr>
-			<tr>
-				<td>Recreation</td>
-				<td>$100</td>
-				<td>$0</td>
-				<td>$100</td>				
-			</tr>
-			<tr>
-				<td>Miscellaneous</td>
-				<td>$100</td>
-				<td>$0</td>
-				<td>$100</td>				
-			<tr>
-			<tr>
-				<td>Total</td>
-				<td>$1100</td>
-				<td>$0</td>
-				<td>$1100</td>				
-			<tr>
-		</table>
-			
+		<?php
+		$sql = "SELECT * FROM budgetCategory WHERE budget_id = '1'";
+		$budgetSum = 0;
+		$spentSum = 0;
+		$remainingSum = 0;
+		if($result = mysqli_query($connection, $sql)){
+			if(mysqli_num_rows($result) > 0){
+				echo "<table class = 'viewTable'>";
+					echo "<tr>";
+						echo "<th>Category</th>";
+						echo "<th>Budgeted</th>";
+						echo "<th>Spent</th>";
+						echo "<th>Remaining</th>";
+					echo "</tr>";
+				while($row = mysqli_fetch_array($result)){
+					echo"<tr>";
+						echo "<td>" . $row['name'] . "</td>";
+						echo "<td>" . $row['amount_allocated'] . "</td>";
+						echo "<td>" . $row['amount_spent'] . "</td>";
+						$allocated = intval($row['amount_allocated']);
+						$spent = intval($row['amount_spent']);
+						$difference = $allocated - $spent;
+						echo "<td>" .  $difference . "</td>";
+						
+						$budgetSum += $allocated;
+						$spentSum += $spent;
+						$remainingSum += $difference;
+				}
+				echo"<tr>";
+						echo "<td>Total</td>";
+						echo "<td>" . $budgetSum . "</td>";
+						echo "<td>" . $spentSum . "</td>";
+						echo "<td>" . $remainingSum . "</td>";
+				echo"</tr>";
+				echo"</table>";
+			}
+		}
+		?>
+		
 		<br>
 			<h3>Add A Transaction</h3>
 			<form id="addTransaction" class="form-vertical" method="post">
