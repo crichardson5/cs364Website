85aa2248ca1f7a8f238addeb67539e5f0a0da696
crichardson5 <54080403+crichardson5@users.noreply.github.com>
2020-05-21 16:31:54 -0500

    Update viewTransaction.php

--- viewTransaction.php
+++ viewTransaction.php
@@ -3,25 +3,28 @@
 	<head>
 		<meta charset="utf-8">
 			<link rel="stylesheet" href="stylesheet.css">
-				<title>View Your Transactions</title>
+				<title>Transactions</title>
 	</head>
 
 	<body>
 		<div class = "header">
-			<h1>View Transactions</h1>
+			<h1>Transactions</h1>
 			<div class="navBar" align="center">
 				<a href="index.php">Home</a>
-				<a class="active" href="viewCreateBudget.php">View and Create Budget</a>
-				<a class="active" href="viewTransaction.html">View Transactions</a>
+				<a href="viewCreateBudget.php">Manage Budget</a>
+				<a class="active" href="viewTransaction.html">Transactions</a>
 				<a href="logout.php">Logout</a>
 			</div>
 		</div>
 
 	<br><br> 
 
-	<h3>View Your Recent Transactions</h3>
+	<h3>Your Recent Transactions</h3>
 	<br>
 	<?php
+	$connection = new mysqli("localhost", "root", "",
+                         "budget");
+						 
 		$sql = "SELECT * FROM budgetTransaction WHERE transaction_id = '1'";
 		if($result = mysqli_query($connection, $sql)){
 			if(mysqli_num_rows($result) > 0){
@@ -37,7 +40,7 @@
 						echo "<td>" . $row['t_date'] . "</td>";
 						echo "<td>" . $row['category_name'] . "</td>";
 						echo "<td>" . $row['t_amount'] . "</td>";
-
+						echo "<td>" . $row['description'] . "</td>";
 					}
 					echo"</tr>";
 				echo"</table>";
