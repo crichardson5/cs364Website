b4d3c1df14e5743c6df723dc67e8db785cfa257c
Sai-Reddy21 <54084108+Sai-Reddy21@users.noreply.github.com>
2020-05-23 17:32:13 -0600

    logout update

--- logout.php
+++ logout.php
@@ -1,5 +1,13 @@
 <?php
 session_start(); // start (or resume) session
+if (! isset($_SESSION["emailAddress"]) // already authenticated
+    && isset($_POST["emailAddress"], $_POST["password"])) {
+  // query database for  account information
+  $statement = $connection->prepare("UPDATE budget
+                                     SET login = FALSE
+                                     WHERE login = TRUE;");
+  $statement->execute();
+}
 session_destroy();
 header("Location: loginPage364.php");
 ?>
